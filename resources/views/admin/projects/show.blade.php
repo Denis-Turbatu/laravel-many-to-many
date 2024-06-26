d69ffbda9d8fd9be525a19f237cbc6f86b385593
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-26 15:49:26 +0200

    feat: font-size in projects/show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,7 +7,7 @@
     <p>{{ $project->slug }}</p>
     
         @foreach ($project->technologies as $item)
-        <p class="badge" style="background-color:{{$item->color}}">
+        <p class="badge fs-6" style="background-color:{{$item->color}}">
             {{$item->name}}
             </p>
         @endforeach
